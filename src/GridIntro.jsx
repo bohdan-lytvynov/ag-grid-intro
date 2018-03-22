b3df4711a406fbe7451f8b92bec54d322f0dbe5d
bogdanl <bogdanl@wix.com>
2018-03-22 15:34:51 +0200

    Load real data and show in the grid

--- src/GridIntro.jsx
+++ src/GridIntro.jsx
@@ -2,53 +2,45 @@
 
 import React, {Component} from 'react'
 import {AgGridReact} from 'ag-grid-react';
+import './grid-intro.css'
 
 class GridIntro extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      columnDefs: this.createColumnDefs(),
-      rowData: this.createRowData()
+      columnDefs: this.createColumnDefs()
     }
+
   }
 
   onGridReady(params) {
     this.gridApi = params.api;
     this.columnApi = params.columnApi;
 
+    fetch('https://api.spacexdata.com/v2/rockets')
+      .then(response => response.json())
+      .then(data => this.gridApi.setRowData(data))
+
     this.gridApi.sizeColumnsToFit();
   }
 
   createColumnDefs() {
     return [
-      {headerName: "Make", field: "make"},
-      {headerName: "Model", field: "model"},
-      {headerName: "Price", field: "price"}
-    ];
-  }
-
-  createRowData() {
-    return [
-      {make: "Toyota", model: "Celica", price: 35000},
-      {make: "Ford", model: "Mondeo", price: 32000},
-      {make: "Porsche", model: "Boxter", price: 72000}
+      {headerName: "Name", field: "name"},
+      {headerName: "Country", field: "country"},
+      {headerName: "Company", field: "company"},
+      {headerName: "Cost per launch", field: "cost_per_launch"}
     ];
   }
 
   render() {
-    let containerStyle = {
-      height: 115,
-      width: 500
-    };
-
     return (
-      <div style={containerStyle} className="ag-fresh">
+      <div className="ag-fresh">
       <h1>Simple ag-Grid React Example</h1>
       <AgGridReact
       // properties
       columnDefs={this.state.columnDefs}
-      rowData={this.state.rowData}
 
       // events
       onGridReady={this.onGridReady}>
