3a7585a646497c9a6291aa9e67bcfdff2e610189
bogdanl <bogdanl@wix.com>
2018-03-22 12:32:14 +0200

    Minimal code for kickoff

---
+++ src/app.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {render} from "react-dom";
+
+import 'ag-grid-root/dist/styles/ag-grid.css';
+import "ag-grid-root/dist/styles/theme-fresh.css";
+
+import GridIntro from "./GridIntro";
+
+document.addEventListener('DOMContentLoaded', () => {
+  render(
+    <GridIntro/>,
+    document.getElementById('root')
+  );
+})
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: "./src/app.jsx",
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env', 'react']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+          use: [
+            { loader: "style-loader" },
+            { loader: "css-loader" }
+          ]
+      }
+    ]
+  },
+  devtool: 'cheap-source-map',
+  externals: {
+    react: 'React',
+    'react-dom':  'ReactDOM'
+  },
+  resolve: {
+    alias: {
+      "ag-grid-root" : "../node_modules/ag-grid"
+    },
+    extensions: [".js", ".jsx"],
+
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ],
+  devServer: {
+    contentBase: './dist'
+  }
+};
